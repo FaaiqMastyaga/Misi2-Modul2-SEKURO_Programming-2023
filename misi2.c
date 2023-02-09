adfcd8339c3ff9e05fec7b39b7de672e950f0f1d
Rakyan Kay <faaiqmastyaga10@gmail.com>
2023-02-09 07:17:30 +0700

    Update misi2.c -- Menambahkan kerangka program lanjutan (pilihan aksi pelanggan saat di dalam warung setelah warung buka)

--- misi2.c
+++ misi2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 void buka_warung();
+void mau_apa();
+void tampilkan_menu();
 
 int main(){
     int buka;
+    int aksi;
+    // buat variabel array untuk menyimpan menu
     
     do{
         buka_warung();
@@ -14,12 +18,35 @@ int main(){
             // clear terminal
             break;
         }else if(buka != 1){
-            // pilihan tidak tersedia;
-            // klik enter
+            printf("Pilihan tidak tersedia.\n"
+            "Klik enter untuk kembali");
             // clear terminal
             continue;
         }
-
+        // input nama pelanggan --> masukkan ke array pelanggan
+        do{
+            mau_apa();
+            scanf("%d", &aksi);
+            switch(aksi){
+                case 1:
+                    // lihat menu
+                    tampilkan_menu();
+                    printf("Klik enter untuk kembali");
+                    break;
+                case 2:
+                    // beli makanan/minuman
+                    // masukkan id menu --> masukkan id dan nama menu ke array pelanggan
+                    // masukkan jumlah --> masukkan ke array pelanggan
+                    // lakukan operasi perhitungan harga --> masukkan ke array pelanggan
+                    break;
+                case 3:
+                    // keluar dari warung
+                    break;
+                default:
+                    printf("Pilihan tidak tersedia.\n"
+                    "Klik enter untuk kembali");
+            }
+        }while(aksi != 3);
     }while(buka != 2);
 
     return 0;
@@ -30,4 +57,16 @@ void buka_warung(){
     "1. Buka warung (pelanggan masuk)\n"
     "2. Tutup warung (menampilkan riwayat penjualan sejak warung dibuka terakhir kali)\n"
     "Masukkan nomor sesuai dengan pilihan Anda: ");
+}
+
+void mau_apa(){
+    printf("Halo {nama_pelanggan}! Silakan pilih salah satu opsi berikut:\n"
+    "1. Lihat menu\n"
+    "2. Beli makanan/minuman\n"
+    "3. Keluar dari warung\n"
+    "Masukkan nomor sesuai dengan pilihan Anda: ");
+}
+
+void tampilkan_menu(/*parameter menu berupa array*/){
+    // gunakan perulangan untuk menampilkan elemen2 array : ID menu, nama menu, tipe menu (makanan/minuman), harga
 }
